011b0cb4492072a2e7878de6ee403d0bcfb5a79a
Sagor Ahamed <141553618+sagorahamed1@users.noreply.github.com>
2025-01-10 22:32:34 +0600

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
 class Config{
- static int appId = 752493796;
- static String appSign = '48658a3e1cc267916153e4fdb37fd6dfaf2b3ba78fd1f6e81b16b6b64ebde6f0';
+ static int appId = give your app id;
+ static String appSign = 'give your zego app sign';
 }
